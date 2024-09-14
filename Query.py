4f22179b29df01c0f3106545cf58ec3eff164ccf
XBD <1519276452@qq.com>
2024-09-14 11:17:20 +0800

    update

--- Query.py
+++ Query.py
@@ -24,7 +24,7 @@ class Query:
         """
         try:
             # 发送请求
-            response = self.Session.request(method, url, **kwargs)
+            response = self.Session.request(method, self.host + url, **kwargs)
             # 检查响应状态码
             if response.status_code == 200:
                 frame = inspect.currentframe().f_back
@@ -50,7 +50,7 @@ class Query:
         :return: None
         """
         url = f'/main/works/updateUserinfo?aab034={org_code}'
-        res = self.relay_request(method="post", url=self.host + url, json={"aab034": org_code})
+        res = self.relay_request(method="post", url=url, json={"aab034": org_code})
         if res:
             print(res.text)
     
@@ -67,26 +67,88 @@ class Query:
             "aac002": idcard,
             "aac003": name
         }
-        query_res = self.relay_request(method='post', url=self.host + query_url, data=query_data)
+        query_res = self.relay_request(method='post', url=query_url, data=query_data)
         if query_res:
             UserInfo = query_res.json()
             login_data = {
                 "password": UserInfo.get('ua0102'),
                 "username": UserInfo.get('ua0100')
             }
-            login_res = self.relay_request(method='post', url=self.host + login_url, json=login_data, headers={"Content-Type": "application/json"})
+            login_res = self.relay_request(method='post', url=login_url, json=login_data, headers={"Content-Type": "application/json"})
             if login_res:
                 map = login_res.json().get('map')
                 if map:
                     self.Session.headers.update(map)
                     entrydatagrid_url = '/user/s9010202/entrydatagrid'
-                    entrydatagrid_res = self.relay_request(method='post', url=self.host + entrydatagrid_url)
+                    entrydatagrid_res = self.relay_request(method='post', url=entrydatagrid_url)
                     if entrydatagrid_res:
-                        print(entrydatagrid_res.text)
-                
+                        entrydatagrid_data = entrydatagrid_res.json()
+                        print(entrydatagrid_data.get('aab300'))
+                        return entrydatagrid_data.get('aab034')
+
+    def login_main(self, name='岳琳', idcard='230304198302014020', org_code=None):
+        """
+        登录并切换指定机构
+        :param: name 姓名
+        :param: idcard 身份证号
+        :param: org_code 机构代码
+        :return: None
+        """
+        login_res = self.login(name, idcard)
+        # print(login_res)
+        if (login_res and org_code) and login_res != org_code:
+            self.change_org(org_code)
+            self.login(name, idcard)
+    
+    def dyff_query(self):
+        """
+        待遇发放明细查询
+        """
+        dyff_url = '/business/m0027/EntryDatagrid'
+        dyff_data = {
+            "aac002": "23030419730102402X",     # 社会保障号
+            "aae041": "",                 # 开始年月
+            "aae042": "",                 # 终止年月
+            "aac001": "2303000314840",                       # 个人编号
+            "page": 1,
+            "rows": 10
+        }
+        dyff_res = self.relay_request(method='post', url=dyff_url, data=dyff_data)
+        if dyff_res:
+            print(dyff_res.text)
+
+    # def dyff_query(self):
+    #     """
+    #     待遇发放明细查询
+    #     """
+    #     pass
+
+    # def dyff_query(self):
+    #     """
+    #     待遇发放明细查询
+    #     """
+    #     pass
+
+    # def dyff_query(self):
+    #     """
+    #     待遇发放明细查询
+    #     """
+    #     pass
+
+    # def dyff_query(self):
+    #     """
+    #     待遇发放明细查询
+    #     """
+    #     pass
+
+    # def dyff_query(self):
+    #     """
+    #     待遇发放明细查询
+    #     """
+    #     pass
 
 
 if __name__ == "__main__":
     Q = Query()
-    Q.change_org(org_code='23002311')
-    Q.login(name='岳琳', idcard='230304198302014020')
+    Q.login_main(org_code='23030111')
+    Q.dyff_query()
